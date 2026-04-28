1b5f71a4bd500a1e2ef892da6ab316c388f7dbc8
rlaxodns <ktun1008@outlook.com>
2026-04-28 11:31:36 +0800

    new C coding test: sumOfSeries

---
+++ codingTest/sumOfSeries.c
@@ -0,0 +1,40 @@
+/*
+문제 설명
+연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 두 정수 num과 total이 주어집니다. 연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
+
+제한사항
+1 ≤ num ≤ 100
+0 ≤ total ≤ 1000
+num개의 연속된 수를 더하여 total이 될 수 없는 테스트 케이스는 없습니다.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int *solution(int num, int total)
+{
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+
+    int *answer = (int *)malloc((sizeof(int)) * num);
+    int a = total / num;
+    int count = 0;
+
+    if (num % 2 == 0)
+    {
+        for (int i = -(num / 2 - 1); i < num / 2 + 1; i++)
+        {
+            answer[count++] = a + i;
+        }
+        return answer;
+    }
+
+    else
+    {
+        for (int i = -(num / 2); i < num / 2 + 1; i++)
+        {
+            answer[count++] = a + i;
+        }
+        return answer;
+    }
+}
